e2c2e06d97f85456d44824fb1855ac705bdbab51
Abel Ayinla <kraneabel@gmail.com>
2022-07-20 15:28:27 +0100

    updated error status

--- routers/user.router.js
+++ routers/user.router.js
@@ -12,29 +12,34 @@ const router = express.Router();
  */
 const { List, Task, User } = require("../database/models");
 
-router.post("/", (req, res, next) => {
+router.post("/", async (req, res, next) => {
   const { email, password } = req.body;
+  let user = await User.findOne({ email });
 
-  bcrypt
-    .hash(password, 10)
-    .then((hash) => {
-      let newUser = new User({
-        email,
-        password: hash,
-      });
-
-      newUser
-        .save()
-        .then((user) => {
-          res.send({ user });
-        })
-        .catch((err) => {
-          console.log(err);
+  if (!user) {
+    bcrypt
+      .hash(password, 10)
+      .then((hash) => {
+        let newUser = new User({
+          email,
+          password: hash,
         });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+
+        newUser
+          .save()
+          .then((user) => {
+            res.send({ statuse: "SUCCESS", user });
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } else {
+    res.send({ status: "FAILED", message: "User already existing." });
+  }
 });
 
 router.post("/login", (req, res, next) => {
@@ -46,7 +51,10 @@ router.post("/login", (req, res, next) => {
         .compare(password, user.password)
         .then((result) => {
           if (!result) {
-            res.send({ message: "Wrong password, try again." });
+            res.send({
+              status: "FAILED",
+              message: "Wrong password, try again.",
+            });
           } else {
             jwt.sign({ userId: user._id }, process.env.SECRET, (err, token) => {
               if (err) throw err;
@@ -57,7 +65,7 @@ router.post("/login", (req, res, next) => {
         })
         .catch((err) => console.log(err));
     } else {
-      res.send({ message: "User not found!" });
+      res.send({ status: "FAILED", message: "User not found!" });
     }
   });
 });
