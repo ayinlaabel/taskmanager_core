0e95cef77f328eb5e34c33935a486de1705bc158
Abel Ayinla <kraneabel@gmail.com>
2022-10-10 16:56:40 +0100

    Test Pipe-line

--- routers/notify.router.js
+++ routers/notify.router.js
@@ -8,20 +8,22 @@ const { User, Notification } = require("../database/models/index");
 
 webpush.setVapidDetails(
   "mailto:test@test.com",
-  process.env.VAPID_PUBLIC_KEY,
-  process.env.VAPID_PRIVATE_KEY
+  "BI68hqfedr9KhYdnEGYbRKKUYj9ZcUMuxe2j_HmvQzgyXLE5GCPjB3CX8HSHGyWKPjfV0uVtYJbuHbmmqsYyhdI",
+  "aGWh2AMEP6spWq-_ThY5sWFaoDZ1xfza13enbrQtI2M"
 );
 
 const router = express.Router();
 
 router.post("/", (req, res, next) => {
-    const subscription = req.body;
+  const subscription = req.body;
 
-    res.status(201).json({});
+  res.status(201).json({});
 
-    const payload = JSON.stringify({title: "Testing Notification."});
+  const payload = JSON.stringify({ title: "Testing Notification." });
 
-    webpush.sendNotification(subscription, payload).catch(err => console.log(err));
+  webpush
+    .sendNotification(subscription, payload)
+    .catch((err) => console.log(err));
 });
 
 module.exports = router;
